7745bf7dcd071aa2c9583783b462e8ebd3d9b744
Peter Varholak <peter.varholak@gmail.com>
2020-01-10 03:28:28 +0100

    try cec-monitor

--- index.js
+++ index.js
@@ -2,38 +2,36 @@
 
 const fs = require('fs')
 const path = require('path')
-const { CecMonitor, Commander, LogicalAddress, OperationCode, PowerStatus, Remote } = require('hdmi-cec')
+const { CEC, CECMonitor } = require('@senzil/cec-monitor')
 const { VLC } = require('node-vlc-http')
 
-const host = process.env.HOST
-const mediaFolder = process.env.MEDIA_FOLDER
-const password = process.env.PASS
-const port = parseInt(process.env.PORT, 10)
+// const host = process.env.HOST
+// const mediaFolder = process.env.MEDIA_FOLDER
+// const password = process.env.PASS
+// const port = parseInt(process.env.PORT, 10)
 
-const MEDIA_DIR = path.join(__dirname, mediaFolder)
+// const MEDIA_DIR = path.join(__dirname, mediaFolder)
 
-const vlc = new VLC({
-  host,
-  password,
-  port,
-  username: ''
-})
+// const vlc = new VLC({
+//   host,
+//   password,
+//   port,
+//   username: ''
+// })
 
-const monitor = new CecMonitor(undefined, LogicalAddress.TV,)
-const commander = new Commander(monitor)
-const remote = new Remote(monitor)
+// fs.readdirSync(MEDIA_DIR).forEach(file => {
+//   const media = `${MEDIA_DIR}/${file}`
+//   console.log('Adding media to playlist:', media)
+//   vlc.addToQueue(media)
+// })
 
-fs.readdirSync(MEDIA_DIR).forEach(file => {
-  const media = `${MEDIA_DIR}/${file}`
-  console.log('Adding media to playlist:', media)
-  vlc.addToQueue(media)
-})
+// vlc.playlistNext()
 
-remote.on('keypress', evt => {
-  console.log(`user pressed the key "${evt.key}" with code "${evt.keyCode}"`)
-})
+const monitor = new CECMonitor('custom-osdname', {})
 
-commander.setPowerState(PowerStatus.ON)
-commander.monitor.executeOperation(LogicalAddress.TV, OperationCode.REQUEST_ACTIVE_SOURCE)
+monitor.WaitForReady().then(() => console.log('rdy'))
 
-vlc.playlistNext()
+// process.on( 'SIGINT', function() {
+//   vlc.stop();
+//   process.exit();
+// });
