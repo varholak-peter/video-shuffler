383aa8c4284b054fdaf734fe92a89cd1e1aaebe6
Peter Varholak <peter.varholak@gmail.com>
2020-01-13 23:27:23 +0100

    fix cec module & formatting

--- index.js
+++ index.js
@@ -1,116 +1,144 @@
 // @ts-check
 
-const { exec } = require('child_process')
-const fs = require('fs')
-const path = require('path')
-const { CEC, CECMonitor } = require('@senzil/cec-monitor')
-const { VLC } = require('node-vlc-http')
+const { exec } = require("child_process");
+const fs = require("fs");
+const path = require("path");
+const { CEC, CECMonitor } = require("@senzil/cec-monitor");
+const { VLC } = require("node-vlc-http");
 
-const host = process.env.HOST
-const mediaFolder = process.env.MEDIA_FOLDER
-const password = process.env.PASS
-const port = parseInt(process.env.PORT, 10)
+const host = process.env.HOST;
+const mediaFolder = process.env.MEDIA_FOLDER;
+const password = process.env.PASS;
+const port = parseInt(process.env.PORT, 10);
 
-const MEDIA_DIR = path.join(__dirname, mediaFolder)
+const MEDIA_DIR = path.join(__dirname, mediaFolder);
+
+const sleep = millis => new Promise(resolve => setTimeout(resolve, millis));
 
 const vlc = new VLC({
   host,
   password,
   port,
   tickLengthMs: 1000,
-  username: ''
-})
+  username: ""
+});
 
-const monitor = new CECMonitor('custom-osdname', {})
+const monitor = new CECMonitor("custom-osdname", {});
 
-let currentTime = 0
-let numberCode = ''
-let numberCodeTimeout = null
+let currentTime = 0;
+let isStarted = false;
+let numberCode = "";
+let numberCodeTimeout = null;
 
-const updateStatus = (status) => {
-  currentTime = status.time
-}
+const updateStatus = status => {
+  currentTime = status.time;
+};
 
-const mediaMap = new Map()
+const mediaMap = new Map();
 
 /**
  * @param {string} name string that starts with 00x00 format
  * @returns 4 character id
  */
-const getMediaKey = (name) => `${name[0]}${name[1]}${name[3]}${name[4]}`
+const getMediaKey = name => `${name[0]}${name[1]}${name[3]}${name[4]}`;
 
 fs.readdirSync(MEDIA_DIR).forEach(file => {
-  const media = `${MEDIA_DIR}/${file}`
-  console.log('Adding media to playlist:', media)
-  vlc.addToQueue(media)
-  mediaMap.set(getMediaKey(file), media)
-})
+  const media = `${MEDIA_DIR}/${file}`;
+  console.log("Adding media to playlist:", media);
+  vlc.addToQueue(media);
+  mediaMap.set(getMediaKey(file), media);
+});
 
 const turnOff = () => {
-  vlc.stop()
-  monitor.Stop()
-  exec('shutdown now')
-}
+  vlc.stop();
+  monitor.Stop();
+  exec("shutdown now");
+};
 
 monitor.WaitForReady().then(() => {
-  monitor.SendMessage(null, CEC.LogicalAddress.TV, CEC.Opcode.REPORT_POWER_STATUS, CEC.PowerStatus.ON);
-  monitor.SendMessage(null, null, CEC.Opcode.ACTIVE_SOURCE, monitor.GetPhysicalAddress());
-  vlc.playlistNext()
-})
+  monitor.WriteRawMessage(`on ${CEC.LogicalAddress.TV}`);
+  monitor.WriteRawMessage("as");
+  monitor.WriteMessage(
+    CEC.LogicalAddress.BROADCAST,
+    CEC.LogicalAddress.TV,
+    CEC.Opcode.GIVE_DEVICE_POWER_STATUS
+  );
+});
+
+monitor.on(CECMonitor.EVENTS.REPORT_POWER_STATUS, async ({ data, source }) => {
+  if (isStarted || source !== CEC.LogicalAddress.TV) {
+    return;
+  }
+
+  if (data.str === "ON") {
+    isStarted = true;
+    vlc.playlistNext();
+  }
+
+  if (data.str === "IN_TRANSITION_STANDBY_TO_ON") {
+    isStarted = true;
+    await sleep(8000);
+    vlc.playlistNext();
+  }
+});
 
 monitor.on(CECMonitor.EVENTS.USER_CONTROL_PRESSED, async ({ data }) => {
-  let status = null
+  let status = null;
 
-  if(data.str === 'RIGHT' || data.str === 'FAST_FORWARD') {
-    console.log('FF')
-    status = await vlc.seek(currentTime + 10)
-    updateStatus(status)
-  } else if(data.str === 'LEFT' || data.str === 'REWIND') {
-    console.log('RW')
-    status = await vlc.seek(currentTime - 10)
-    updateStatus(status)
-  } else if(data.str === 'SELECT' || data.str === 'PLAY' || data.str === 'PAUSE') {
-    console.log('Play/PAUSE')
-    status = await vlc.pause()
-    updateStatus(status)
-  } else if(data.str === 'CHANNEL_UP' || data.str === 'UP') {
-    console.log('playlist next')
-    status = await vlc.playlistNext()
-  } else if(data.str === 'CHANNEL_DOWN' || data.str === 'DOWN') {
-    console.log('playlist previous')
-    status = await vlc.playlistPrevious()
-  } else if(data.str.startsWith('NUMBER')) {
-    clearTimeout(numberCodeTimeout)
-    numberCode += data.str[6]
+  if (data.str === "RIGHT" || data.str === "FAST_FORWARD") {
+    console.log("FF");
+    status = await vlc.seek(currentTime + 10);
+    updateStatus(status);
+  } else if (data.str === "LEFT" || data.str === "REWIND") {
+    console.log("RW");
+    status = await vlc.seek(currentTime - 10);
+    updateStatus(status);
+  } else if (
+    data.str === "SELECT" ||
+    data.str === "PLAY" ||
+    data.str === "PAUSE"
+  ) {
+    console.log("Play/PAUSE");
+    status = await vlc.pause();
+    updateStatus(status);
+  } else if (data.str === "CHANNEL_UP" || data.str === "UP") {
+    console.log("playlist next");
+    status = await vlc.playlistNext();
+  } else if (data.str === "CHANNEL_DOWN" || data.str === "DOWN") {
+    console.log("playlist previous");
+    status = await vlc.playlistPrevious();
+  } else if (data.str.startsWith("NUMBER")) {
+    clearTimeout(numberCodeTimeout);
+    numberCode += data.str[6];
 
     if (numberCode.length < 4) {
       numberCodeTimeout = setTimeout(() => {
-        numberCode = ''
-      }, 3000)
-      return
+        numberCode = "";
+      }, 3000);
+      return;
     }
 
-    if (numberCode === '9999') {
-      console.log('toggleRandom')
-      status = await vlc.toggleRandom()
-    } else if (numberCode === '0000') {
-      turnOff()
+    if (numberCode === "9999") {
+      console.log("toggleRandom");
+      status = await vlc.toggleRandom();
+    } else if (numberCode === "0000") {
+      turnOff();
     } else if (mediaMap.has(numberCode)) {
-      console.log('play:', mediaMap.get(numberCode))
-      status = await vlc.addToQueueAndPlay(mediaMap.get(numberCode))
+      console.log("play:", mediaMap.get(numberCode));
+      status = await vlc.addToQueueAndPlay(mediaMap.get(numberCode));
     }
 
-    numberCode = ''
+    numberCode = "";
   }
 
   if (status) {
-    updateStatus(status)
+    updateStatus(status);
   }
-})
+});
 
-vlc.on('update', updateStatus);
+vlc.on("update", updateStatus);
 
-process.on('SIGINT', function() {
+process.on("SIGINT", function() {
   vlc.stop();
   process.exit();
 });
